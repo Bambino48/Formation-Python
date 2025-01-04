6c3bbff443b519e68666b0c4152746c7453510fc
Bambino48 <ibamba366@gmail.com>
2025-01-04 21:19:17 +0000

    Utilisation de la fonction input

---
+++ modules_4/main.py
@@ -0,0 +1,8 @@
+""" Demander des informations à l'utilisateur
+
+Pour recuperer la saisir on utiliser la fonction input() entrant un message de l'information que l'on veut de
+l'utilisateur.
+"""
+
+nombre = input("Entrez un nombre : ")
+print("Vous avez saisi " + nombre +".")
