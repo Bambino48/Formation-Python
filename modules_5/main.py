52acbbe25f742b1b36dd5a9f55f18a6654a623aa
Bambino48 <ibamba366@gmail.com>
2025-01-04 21:55:52 +0000

    Methode de resoudre un probleme

---
+++ modules_5/main.py
@@ -0,0 +1,20 @@
+""" Apprendre à résoudre des problèmes
+
+- faire des commentaires en français du problème
+- Exemple : un programme de faire l'addition de deux nombres saisir par l'utilisateur
+            et afficher le resultat.
+
+                feuille de route
+
+            Demander un premier nombre à l'utilisateur
+            Demander un deuxième nombre à l'utilisateur
+            Additionner les deux nombres ensemble
+            Afficher le résultat à l'utilisateur
+
+
+- Pour résoudre un problème, il faut lire la documentation
+"""
+
+a = input("Entrer un nombre : ")
+b = input("Entrer un nombre : ")
+
