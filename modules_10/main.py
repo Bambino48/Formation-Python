2e0d9981fb3f632a199a1c85bee48a53a2baeb6c
Bambino48 <ibamba366@gmail.com>
2025-01-07 12:47:48 +0000

    Les erreurs en programmation python

---
+++ modules_10/main.py
@@ -0,0 +1,21 @@
+"""
+Les erreurs en python  – Les erreurs de syntaxes
+                       — les erreurs d'executions
+                       — les erreurs semantique
+"""
+
+# Les erreurs de syntaxes
+""""
+For i in range(10):
+    print(i)
+"""
+# la sensibilite a la casse
+# les deux-points
+# L'utilisation de mot reserves
+# Les guillemets
+
+# Les erreurs d'execution (RuntimeError)
+    # NameError (variable non definie)
+    # TypeError
+
+# Les erreurs semantiques (les erreurs de logiques)
